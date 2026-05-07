8c7795dc9813145e5e70136e375581651fd74a3c
Janis Karklins <janis.karklins88@gmail.com>
2026-05-07 09:51:23 +0300

    Initial project setup

---
+++ src/main/java/com/jk/paper_trading_dashboard/PaperTradingDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.jk.paper_trading_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperTradingDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperTradingDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/paper_trading_dashboard/PaperTradingDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.paper_trading_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperTradingDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
